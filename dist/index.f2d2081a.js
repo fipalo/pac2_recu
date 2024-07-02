21e1dce056af7b40a9db43fc48a366f93b8f42a6
fipalo <fionaparis99@gmail.com>
2024-07-02 18:18:44 +0200

    3

---
+++ dist/index.f2d2081a.js
@@ -0,0 +1,2 @@
+$(window).scroll(function(){$(this).scrollTop()>350?($(".icon-bar").addClass("opaque"),$(".navbar-brand img").attr("src","img/logo-light.png")):($(".icon-bar").removeClass("opaque"),$(".navbar-brand img").attr("src","img/logo.png"))}),$(function(){$(window).scroll(function(){$(this).scrollTop()>100&&($("#topbar, .cart-label").fadeOut("slow"),$(".logo img").attr("src","img/logo.png")),100>$(this).scrollTop()&&($("#logo, #topbar, .cart-label").fadeIn("fast"),$(".logo img").attr("src","img/logo-light.png"))})});
+//# sourceMappingURL=index.f2d2081a.js.map
